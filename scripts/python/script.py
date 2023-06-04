572c3a208c7d4b0fb9a431d07483733d68906d1d
vpuzin <vpuzin@codemastersintl.com>
2023-06-04 23:27:43 +0300

    fix date

--- scripts/python/script.py
+++ scripts/python/script.py
@@ -2,13 +2,13 @@ import pandas as pd
 import plotly.express as px
 from sklearn.ensemble import IsolationForest
 
-dataset = pd.read_csv('https://hands-on.cloud/wp-content/uploads/2022/02/catfish_sales_1986_2001.csv', parse_dates=[0])
+dataset = pd.read_csv('http://127.0.0.1:8000/anomalies/download/4007', parse_dates=[0])
 
-datetime_series = pd.to_datetime(dataset['Date'])
+datetime_series = pd.to_datetime(dataset['period_date'])
 datetime_index = pd.DatetimeIndex(datetime_series.values)
 period_index = pd.PeriodIndex(datetime_index, freq='M')
 dataset = dataset.set_index(period_index)
-dataset.drop('Date',axis=1,inplace=True)
+dataset.drop('period_date',axis=1,inplace=True)
 dataset.head()
 
 isf_dataset = dataset.copy()
@@ -21,4 +21,4 @@ df = pd.DataFrame(anomalies)
 
 print(df)
 
-df.to_csv(r'F:\Отчеты\analyze\anomalies.csv', index=False, header=True)
+df.to_csv(r'F:\Отчеты\collector\scripts\python\anomalies.csv', index=False, header=True)
